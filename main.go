ec980812933bb3c3500ac91b8847854887b5ce73
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-04-28 16:02:14 +0100

    walk trough base dir func

--- main.go
+++ main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
+	"path/filepath"
 
 	tree_sitter "github.com/tree-sitter/go-tree-sitter"
 	tree_sitter_javascript "github.com/tree-sitter/tree-sitter-javascript/bindings/go"
 )
 
+func FileWalker(dir string) ([]string, error) {
+    var files []string
+
+    err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        if !info.IsDir() {
+            files = append(files, path)
+        }
+        return nil
+    })
+
+    if err != nil {
+        return nil, err
+    }
+
+    return files, nil
+}
 
 func main(){
 
@@ -20,7 +41,15 @@ func main(){
 
 	tree := parser.Parse(code,nil)
 	defer tree.Close()
+	
+	files, err := FileWalker("./")
+	if err != nil {
+		fmt.Println("Error walking the directory:", err)
+		return
+	}
 
-	root := tree.RootNode()
-	fmt.Println(root.ToSexp())
+	for _, file := range files {
+		fmt.Println(file)
+	}
+
 }
