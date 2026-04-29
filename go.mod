92e43a08c283513142b6792b4ec832bd193c3319
Ricardo-Ceia <A49000@alunos.isel.pt>
2026-04-29 23:14:20 +0100

    (feat): parser wrapper

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module Niflheim
 go 1.24.1
 
 require (
+	github.com/smacker/go-tree-sitter v0.0.0-20240827094217-dd81d9e9be82
 	github.com/tree-sitter/go-tree-sitter v0.25.0
+	github.com/tree-sitter/tree-sitter-go v0.23.4
+	github.com/tree-sitter/tree-sitter-java v0.23.5
 	github.com/tree-sitter/tree-sitter-javascript v0.25.0
+	github.com/tree-sitter/tree-sitter-python v0.23.6
 )
 
 require github.com/mattn/go-pointer v0.0.1 // indirect
--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 	"io/fs"
 	"path/filepath"
 	"strings"
+	"os"
 
-	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter            "github.com/tree-sitter/go-tree-sitter"
 	tree_sitter_javascript "github.com/tree-sitter/tree-sitter-javascript/bindings/go"
+	tree_sitter_python     "github.com/tree-sitter/tree-sitter-python/bindings/go"
+	tree_sitter_go         "github.com/tree-sitter/tree-sitter-go/bindings/go"
+	tree_sitter_java       "github.com/tree-sitter/tree-sitter-java/bindings/go"
 )
 
 func LangDetector(filePath string) (string, error) {
@@ -27,6 +31,39 @@ func LangDetector(filePath string) (string, error) {
 	default:
 		return "", fmt.Errorf("unsupported file type: %s", ext)
 	}
+}
+
+func ParserWrapper(filePath string) (*tree_sitter.Tree, error) {
+	lang, err := LangDetector(filePath)
+	if err != nil {
+		// If the file type is unsupported, we can choose to skip it without returning an error
+		fmt.Printf("Skipping unsupported file type: %s\n", filePath)
+		return nil, nil
+	}
+	
+	parser := tree_sitter.NewParser()
+	defer parser.Close()	
+	
+	switch lang {
+	case "javascript":
+		parser.SetLanguage(tree_sitter.NewLanguage(tree_sitter_javascript.Language()))
+	case "python":
+		parser.SetLanguage(tree_sitter.NewLanguage(tree_sitter_python.Language()))
+	case "go":
+		parser.SetLanguage(tree_sitter.NewLanguage(tree_sitter_go.Language()))
+	case "java":
+	 	parser.SetLanguage(tree_sitter.NewLanguage(tree_sitter_java.Language()))
+	default:
+		return nil, fmt.Errorf("unsupported language: %s", lang)
+	}
+
+	code, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+
+	tree := parser.Parse(code, nil)
+	return tree, nil
 }
 
 func FileWalker(dir string) ([]string, error) {
@@ -75,7 +112,14 @@ func main(){
 	}
 
 	for _, file := range files {
-		fmt.Println(file)
+		tree, err := ParserWrapper(file)
+		if err != nil {
+			fmt.Printf("Error parsing file %s: %v\n", file, err)
+			continue
+		}
+		if tree != nil {
+			fmt.Printf("Successfully parsed file: %s\n", file)
+			tree.Close() // Don't forget to close the tree after use
+		}
 	}
-
 }
